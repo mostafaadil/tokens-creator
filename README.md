42540795a8a874e18926fafcaa0e7421f7818c29
mostafaadil <mostafaadil58@gmail.com>
2022-09-27 00:51:43 -0700

    uploading the package

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# tokens-creator" 
+<html>
+<h1>to install the package</h1>
+<h2>npm i tokens-creator</h2>
+
+<br>
+<h1>to install the tokens creator</h1>
+<h2>npm i tokens-creator</h2>
+
+<br>
+<h1>to use  the tokens creator</h1>
+<h2>const createToken=require('tokens-creator')</h2>
+
+</html>
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const createToken = () => {
+const createToken = (tokenLength) => {
     var text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910#$%^&*+-~!@><";
     var token = "";
     const range = 1000;
-    var time = new Date.now();
-    console.log(time)
-    for (var i = 0; i < 1000; i++) {
+  //  console.log(time)
+    for (var i = 0; i <tokenLength; i++) {
         var index = Math.floor(Math.random() * (text.length - 1 + 0) + 0);
-        var temp = Math.floor(Math.random() * (time.length - 1 + 0) + 0)
-        token += text[index] + temp;
+        var time = new Date().getMilliseconds();
+        token += text[index] + time;
     }
     return token
 }
